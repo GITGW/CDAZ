7b45f81b72420b75c7ea819ea0b4c0b77a5d5103
GITGW <grahamwhiteley@msn.com>
2018-12-07 12:56:16 -0600

    Add files via upload

---
+++ IdentityChoice.ps1
@@ -0,0 +1,15 @@
+﻿
+  
+  if ($identitystore -icontains 'aad') 
+  {Set-Location D:\DeploymentFiles\Automation
+  .\VSTSDellEMCInstallAzS-AAD.ps1
+  Write-Host -ForegroundColor Yellow "You are deploying using IdentityStore $identitystore "
+   }
+  
+                    
+ else 
+            { Set-Location D:\DeploymentFiles\Automation
+            .\VSTSDellEMCInstallAzS-ADFS.ps1
+                
+ }
+ Write-Host -ForegroundColor Yellow "You are deploying using IdentityStore $identitystore"             
